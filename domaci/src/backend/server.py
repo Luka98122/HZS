f0f1523a7aa4a6d2edc2c0983ce6a8751ef6a036
luka98122 <luka98122@gmail.com>
2026-01-11 15:25:58 +0100

    Example API

--- domaci/src/backend/server.py
+++ domaci/src/backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
-
+import time
+import datetime
 app = Flask(__name__)
 
 # Za sad deployed na
@@ -8,6 +9,19 @@ app = Flask(__name__)
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify(status="ok"), 200
+
+@app.route("/time", methods=["GET"])
+def time_rn():
+    time_rn = time.time()
+    time_later = time.time()+5
+    date_rn = datetime.datetime.now()
+    result = {
+        "text" : f"The time right now is {time_rn}.",
+        "Future" : f"Five seconds into the future is {time_later}",
+        "Date" : f"The date is: {date_rn}",
+        "status" : "ok"
+    }
+    return jsonify(result), 200 # HTTP code 200 = OKs
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
